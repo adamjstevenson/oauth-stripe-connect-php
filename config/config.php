5eb44303db487193b5ce9ee5cb58f1c34e468052
Adam Stevenson <adam@stripe.com>
2016-01-10 15:25:38 -0700

    fix redirect URL

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 
 define("CLIENT_ID", "ca_7dsxjCPZ3Ki753aji7elle6doG2Zzp1C"); // Your client ID: https://dashboard.stripe.com/account/applications/settings
 define("SECRET_KEY","sk_test_your_secret_key"); // Your secret API KEY: https://dashboard.stripe.com/account/apikeys
-define("REDIRECT_URL", "http://12.12.12.12/connect/"); // https://dashboard.stripe.com/account/applications/settings
+define("REDIRECT_URL", "http://12.12.12.12/connected.php"); // https://dashboard.stripe.com/account/applications/settings
 
 \Stripe\Stripe::setApiKey(SECRET_KEY);
 ?>
