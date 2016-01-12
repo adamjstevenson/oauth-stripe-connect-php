e2b53bf3e0a1c0ec099ef9550c13ecb0ba589482
Adam Stevenson <adam@stripe.com>
2016-01-11 18:53:58 -0700

    clean up nesting

--- oauth/connect.php
+++ oauth/connect.php
@@ -15,37 +15,36 @@ $provider = new \League\OAuth2\Client\Provider\GenericProvider([
 
 // Check for an authorization code
 if (isset($_GET['code'])){
-	$code = $_GET['code'];
+  $code = $_GET['code'];
 
-	// Try to retrieve the access token
-	try {
-		$accessToken = $provider->getAccessToken('authorization_code', 
-			['code' => $_GET['code']
+  // Try to retrieve the access token
+  try {
+    $accessToken = $provider->getAccessToken('authorization_code', 
+      ['code' => $_GET['code']
     ]);
 
     // You could retrieve the API key with `$accessToken->getToken()`, but it's better to authenticate using the Stripe-account header (below)
 
-		// Retrieve the account ID to be used for authentication: https://stripe.com/docs/connect/authentication
-		// TODO: Save this account ID to your database for later use.
-		$account_id = $provider->getResourceOwner($accessToken)->getId();
+    // Retrieve the account ID to be used for authentication: https://stripe.com/docs/connect/authentication
+    // TODO: Save this account ID to your database for later use.
+    $account_id = $provider->getResourceOwner($accessToken)->getId();
 		
-		// Retrieve the account from Stripe: https://stripe.com/docs/api/php#retrieve_account
-		$account = \Stripe\Account::Retrieve($account_id);
+    // Retrieve the account from Stripe: https://stripe.com/docs/api/php#retrieve_account
+    $account = \Stripe\Account::Retrieve($account_id);
 
-		$success = "Your Stripe account has been connected!";
-
-	}
-	catch (Exception $e){
-		$error = $e->getMessage();
-	}
+    $success = "Your Stripe account has been connected!";
+  }
+  catch (Exception $e){
+    $error = $e->getMessage();
+  }
 }
 // Handle errors
 elseif (isset($_GET['error'])){
-	$error = $_GET['error_description'];
+  $error = $_GET['error_description'];
 }
 // No authorization code -- display an error, etc. 
 else {
-	$error = "No authorization code received";
+  $error = "No authorization code received";
 }
 
 ?>
