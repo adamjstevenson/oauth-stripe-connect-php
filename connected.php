b2105d8791676916b7d649226410595f90868469
Adam Stevenson <adam@stripe.com>
2016-01-11 18:43:02 -0700

    clean up nesting a bit

--- connected.php
+++ connected.php
@@ -9,26 +9,24 @@
     <?php require_once("views/site/nav.php"); ?>
 
     <div class="container">
-      
+
       <!-- Display a message to let the user know they're connected -->
       <?php if (isset($success)): ?>
-      	<div class="alert alert-success">
-      	  <strong><?php echo $success; ?></strong>
-      	</div>
+        <div class="alert alert-success">
+          <strong><?php echo $success; ?></strong>
+        </div>
       <?php elseif (isset($error)): ?>
-      	<div class="alert alert-danger">
-      	  <strong><?php echo $error; ?></strong>
-      	</div>
+        <div class="alert alert-danger">
+          <strong><?php echo $error; ?></strong>
+        </div>
       <?php endif ?>
 
       <!-- Display some info about the account YAY FUN -->
       <?php if (isset($account)): ?>
         <h2 class="page-header">About your account</h2>
-      	<div class="panel panel-default shadow-sm">
-      		<div class="panel-body">
-
-      			<dl class="dl-horizontal">
-
+        <div class="panel panel-default shadow-sm">
+          <div class="panel-body">
+            <dl class="dl-horizontal">
               <dt><i class="fa fa-cc-stripe"></i> Account ID</dt>
               <dd><?= $account->id; ?></dd>
 
@@ -47,7 +45,7 @@
               <dt><i class="fa fa-briefcase"></i> Display Name</dt>
               <dd><?= $account->display_name; ?></dd>
 
-      				<dt><i class="fa fa-file-text"></i> Statement Descriptor</dt>
+              <dt><i class="fa fa-file-text"></i> Statement Descriptor</dt>
               <dd><?= $account->statement_descriptor; ?></dd>
 
               <dt><i class="fa fa-question-circle"></i> Charges Enabled</dt>
@@ -56,14 +54,13 @@
               <dt><i class="fa fa-question-circle"></i> Transfers Enabled</dt>
               <dd><?= $account->transfers_enabled ? 'True' : 'False'; ?></dd>
 
-      				<dt><i class="fa fa-link"></i> Website</dt>
-      				<dd><?= $account->business_url; ?></dd>
-
-      			</dl>
-      		</div>
-      	</div>
+              <dt><i class="fa fa-link"></i> Website</dt>
+              <dd><?= $account->business_url; ?></dd>
+            </dl>
+          </div>
+        </div>
       <?php endif ?>
-
+      
     </div>
 
     <?php require_once("views/site/footer.php"); ?>
