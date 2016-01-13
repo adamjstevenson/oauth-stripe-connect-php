e9b8b7832bacd231818589a30c79414c3c7b5214
Adam Stevenson <adam@stripe.com>
2016-01-12 17:47:23 -0700

    merge everything into config file

--- config/config.php
+++ config/config.php
@@ -1,5 +1,7 @@
 <?php 
 
+require_once($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
+
 define("CLIENT_ID", "ca_your_client_id"); // Your client ID: https://dashboard.stripe.com/account/applications/settings
 define("SECRET_KEY","sk_your_secret_key"); // Your secret API KEY: https://dashboard.stripe.com/account/apikeys
 define("REDIRECT_URL", "http://12.12.12.12/connected.php"); // https://dashboard.stripe.com/account/applications/settings
--- oauth/connect.php
+++ oauth/connect.php
@@ -1,6 +1,4 @@
 <?php 
-
-require_once($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/config/config.php');
 
 // initialize a new generic provider
