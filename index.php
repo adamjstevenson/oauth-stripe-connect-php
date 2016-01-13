dca58312e0b8de2243ad09d4b38823c3e0409978
Adam Stevenson <adam@stripe.com>
2016-01-12 17:47:56 -0700

    link the client ID from the config file

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php require_once("config/config.php"); ?>
+
 <!DOCTYPE html>
 <html lang="en">
   
@@ -18,7 +20,7 @@
         This integration uses <a href="https://stripe.com/docs/connect/standalone-accounts">standalone accounts</a>, so you can either create a new test account using <a href="https://stripe.com/docs/testing">some test data</a>, or connect an existing account (in test mode).
       </p>
       <p>
-        <a class="btn btn-primary btn-block btn-xl" data-loading-text="Connecting..." href="https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_7dsxjCPZ3Ki753aji7elle6doG2Zzp1C&scope=read_write">Connect to Stripe</a>
+        <a class="btn btn-primary btn-block btn-xl" data-loading-text="Connecting..." href="https://connect.stripe.com/oauth/authorize?response_type=code&client_id=<?php echo CLIENT_ID; ?>&scope=read_write">Connect to Stripe</a>
       </p>
     </div>
 
