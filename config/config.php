80cd182edd2408faf0ae2ec2e71e2064cea32fe3
Adam Stevenson <adam@stripe.com>
2016-01-12 17:34:24 -0700

    add placeholder client ID

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php 
 
-define("CLIENT_ID", "ca_7dsxjCPZ3Ki753aji7elle6doG2Zzp1C"); // Your client ID: https://dashboard.stripe.com/account/applications/settings
+define("CLIENT_ID", "ca_your_client_id"); // Your client ID: https://dashboard.stripe.com/account/applications/settings
 define("SECRET_KEY","sk_your_secret_key"); // Your secret API KEY: https://dashboard.stripe.com/account/apikeys
 define("REDIRECT_URL", "http://12.12.12.12/connected.php"); // https://dashboard.stripe.com/account/applications/settings
 
